6b581ee8172d94fd8f7e0bde93a3d8167e826d8e
wbolger23 <walker.bolger@gmail.com>
2020-04-28 13:37:00 -0700

    Added files and changes required for Heroku deployment

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/walkerbolger/dev/ringerz/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- ringerz/settings.py
+++ ringerz/settings.py
@@ -20,11 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '!8s!8efksc$-k6yc=abuez2a5z6zx)92-)l1-3o5)r63hpn#tb'
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '!8s!8efksc$-k6yc=abuez2a5z6zx)92-)l1-3o5)r63hpn#tb')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != False
 
 ALLOWED_HOSTS = ['*']
@@ -49,6 +47,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -125,14 +124,25 @@ USE_L10N = False
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-STATIC_URL = '/static/'
-
 # Redirect to home URL after login (default redirect to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/'
 
-# DATE_INPUT_FORMATS = ['%m/%d/%Y']
-# TIME_INPUT_FORMATS = ['%I:%M %p']
 DATETIME_INPUT_FORMATS = ['%m/%d/%Y %I:%M %p']
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.1/howto/static-files/
+
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# The URL to use when referring to static files (where they will be served from)
+STATIC_URL = '/static/'
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
