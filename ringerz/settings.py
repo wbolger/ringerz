c4b5a2b4273881f0430ab7806ab7b9c9dbb4e792
wbolger23 <walker.bolger@gmail.com>
2020-04-28 14:25:53 -0700

    Updated ALLOWED_HOSTS with site and dev server.

--- ringerz/settings.py
+++ ringerz/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '!8s!8efksc$-k6yc=abuez2a5z6zx)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['ringerz.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
